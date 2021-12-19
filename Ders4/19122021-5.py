2ea5eca13f9f246caac5f01cff3c707466a0ac6e
Saygın ÖZTUNÇ <sayginoztunc@gmail.com>
2021-12-19 14:06:44 +0300

    Delete Ders4 directory

--- Ders4/19122021-5.py
+++
@@ -1,3 +0,0 @@
-while True:
-    # 5==5, 5<=5, "a"=="a", "a" in "ahmet" vs hepsi True üretir
-    print("BTK")
--- Ders4/19122021-7.py
+++
@@ -1,4 +0,0 @@
-for i in range(100):
-    if i==15:
-        continue #gördüğü anda döngünün bu adımı burada sonlanır, diğer adıma geçer
-    print(i)
