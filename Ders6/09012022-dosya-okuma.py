65fda49df18455cabc27c5409ddce2fd78a4d937
Saygın ÖZTUNÇ <sayginoztunc@gmail.com>
2022-03-06 15:51:42 +0300

    Delete Ders6 directory

--- Ders6/09012022-dosya-okuma.py
+++
@@ -1,8 +0,0 @@
-import time
-dosya=open("bilgiler.txt","r",encoding="utf-8")
-icerik=dosya.read()
-print("içerik okunuyor...")
-
-time.sleep(3)
-print(icerik)
-dosya.close()
--- Ders6/uygulama-2.py
+++
@@ -1,6 +0,0 @@
-dosya=open("sayilar.txt","w",encoding="utf-8")
-for i in range(1,10000):
-    dosya.write(str(i)+"\n")
-print("Bilgiler başarıyla işlendi...")
-dosya.close()
-#pip install gTTs --> yeni kütüphane eklemek için terminale yazılır.. gTTs: kütüphane adı
